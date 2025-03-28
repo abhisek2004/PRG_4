368133ef4a23e0aace98f32ce6ab55f97401e4b7
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2025-03-28 14:37:35 +0530

    remove unused images, configuration files, and components

--- onlinecodecompiler/prisma/migrations/20241122065650_sharecode_schema/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "ShareCode" (
-    "id" TEXT NOT NULL,
-    "code" TEXT NOT NULL,
-    "language" TEXT NOT NULL,
-
-    CONSTRAINT "ShareCode_pkey" PRIMARY KEY ("id")
-);
